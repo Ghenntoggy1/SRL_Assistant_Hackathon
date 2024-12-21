bd2c75b4beff5234ef9b949f99d1641b2d35e648
Ghenntoggy1 <roman.gusev@isa.utm.md>
2024-12-21 14:11:11 +0200

    git init

---
+++ src/main/java/com/srl_assistant/srl_assistant/SrlAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.srl_assistant.srl_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrlAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrlAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/srl_assistant/srl_assistant/SrlAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srl_assistant.srl_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrlAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
